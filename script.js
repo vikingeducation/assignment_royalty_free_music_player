c91fbe7a3f0a56e99f1d1634d233992fafd656c7
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-16 11:01:09 +0000

    Add listener for play-buttons

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function() {
     musicPlayer.init();
+    //Whenever a play button is clicked, get the respective audio for that play-button and call play() on it
+    //Should we delegate so that we don't need to worry about toggling? Let's try
+    $(".play-button").on("click", function(event) {
+        let audioElement = $(event.currentTarget).parents(".music-buttons").siblings("audio").get(0); //get the music-buttons containers of the clicked button, find the audio element that's a sibling of that button, then get the DOMElement back.
+        audioElement.play(); //Call play() on HTMLAudioElement (MediaElement)
+        
+    });
 });
 
 let musicPlayer = {
