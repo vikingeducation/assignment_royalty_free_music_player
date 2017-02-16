689af38a803dffc2e4cde37de590fc2fb856dd45
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-16 11:28:10 +0000

    Make event delegation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let musicPlayer = {
     "init" : function init() {
     //Whenever a play button is clicked, get the respective audio for that play-button and call play() on it
     //Should we delegate so that we don't need to worry about toggling? Let's try
-        $(".play-button").on("click", function(event) {
+        $(".play-pause-button").on("click", ".play-button", function(event) {
         let $musicButton = $(event.currentTarget);
         let audioElement = $musicButton.parents(".music-buttons").siblings("audio").get(0); //get the music-buttons containers of the clicked button, find the audio element that's a sibling of that button, then get the DOMElement back.
         $musicButton.addClass("pause-button").removeClass("play-button"); //Switch button types
@@ -16,7 +16,7 @@ let musicPlayer = {
         });
         
         
-        $(".pause-button").on("click", function(event) {
+        $(".play-pause-button").on("click", ".pause-button", function(event) {
         let $musicButton = $(event.currentTarget);
         let audioElement = $musicButton.parents(".music-buttons").siblings("audio").get(0); //get the music-buttons containers of the clicked button, find the audio element that's a sibling of that button, then get the DOMElement back.
         $musicButton.addClass("play-button").removeClass("pause-button"); //Switch button types
