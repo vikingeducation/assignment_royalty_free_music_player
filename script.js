d33add32078c681297527d699fb03aa5bec1fdad
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-17 03:31:43 +0000

    Reorder function calls in init

--- script.js
+++ script.js
@@ -19,15 +19,14 @@ let musicPlayer = {
             audioElement.play();
         }
         $musicButton.addClass("pause-button").removeClass("play-button"); //Switch button types
-        audioElement.play(); //Call play() on HTMLAudioElement (MediaElement)
         });
         
         
         $(".play-pause-button").on("click", ".pause-button", function(event) {
         let $musicButton = $(event.currentTarget);
         let audioElement = $musicButton.parents(".music-buttons").siblings("audio").get(0); //get the music-buttons containers of the clicked button, find the audio element that's a sibling of that button, then get the DOMElement back.
-        $musicButton.addClass("play-button").removeClass("pause-button"); //Switch button types
         audioElement.pause(); //Call pause() on HTMLAudioElement (MediaElement)
+        $musicButton.addClass("play-button").removeClass("pause-button"); //Switch button types
         });
 
     },
