b6ce87d62a86cdbb32d0f7fe87a7317cc31580f8
Adam Jazairi <adamjazairi@gmail.com>
2017-03-09 20:57:52 -0500

    First stab at activating controls

--- scripts.js
+++ scripts.js
@@ -1,9 +1,41 @@
 $(document).ready(function() {
-  player.init();
+  player.toggleAudio();
+  player.mainControls();
 });
 
 var player = {
-  "init": function init() {
-    
+  "toggleAudio": function() {
+    $(".track-toggle").on("click", function() {
+      var audio = $(this).prev('audio');
+      if ($(this).hasClass("now-playing")) {
+        audio.play();
+        $(this).addClass("now-playing")
+          .removeClass("fa-play")
+          .addClass("fa-pause");
+        var currentMetadata = $(this).parent().next().clone();
+        $("footer > div.track-metadata").replaceWith(currentMetadata);
+      }
+      else {
+        audio.pause();
+        $(this).addClass("now-playing")
+          .removeClass("fa-play")
+          .addClass("fa-pause");
+      }
+    });
+  },
+
+  "mainControls": function() {
+    $("#main-toggle").on("click", function() {
+      var audio = $(".now-playing").next('audio');
+      if ($(this).hasClass("fa-play")) {
+        audio.play();
+        $(this).addClass("fa-pause").removeClass("fa-play");
+        $(".now-playing").addClass("fa-pause").removeClass("fa-play");
+      } else {
+        audio.pause();
+        $(this).addClass("fa-play").removeClass("fa-pause");
+        $(".now-playing").addClass("fa-play").removeClass("fa-pause");
+      }
+    });
   }
 };
