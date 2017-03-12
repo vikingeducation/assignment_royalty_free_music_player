f00073bfa7c2b05777ebe5e6ea9bfa02964e5ac0
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-12 15:06:22 -0700

    Add basic play/pause functionality via clicking songs in list

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,41 @@
-( function ($ ) {
+$(document).ready(function() {
   "use strict";
-  $(document).ready(function() {
 
+    var $songContainers = $(".song-container");
+    var $allSongs = $("audio");
 
+    $songContainers.click(function(event) {
+      var $target = $(this);
+      var $playButton = $target.children(".glyphicon-play");
+      var $pauseButton = $target.children(".glyphicon-pause");
+      var song = $target.children("audio")[0];
+
+      //if the clicked div is currently playing (so play button is hidden)
+      if ($playButton.hasClass("inactive")) {
+        //Hide the pause button, show the play button (song isn't playing)
+        $playButton.removeClass("inactive");
+        $pauseButton.addClass("inactive");
+
+        $allSongs.each(function( index, element ) {
+          this.pause();
+        });
+      }
+      //if the clicked div is not currently playing (so pause button is hidden)
+      else {
+        //find any currently playing songs (their play button will be hidden)
+        $(".glyphicon-pause").addClass("inactive");
+        $(".glyphicon-play").removeClass("inactive");
+
+        $playButton.addClass("inactive");
+        $pauseButton.removeClass("inactive");
+
+        $allSongs.each(function( index, element ) {
+          this.pause();
+        });
+        
+        song.play();
+
+      }
+
+    });
   });
-} ( jQuery ) );
