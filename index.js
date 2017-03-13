9d67306f82eb2de2303ae141e5bd18c6cb4a18a0
tim5046 <tim5046@gmail.com>
2017-03-13 16:44:56 -0700

    wire up play/pause and text in footer

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ var playHandlers = {
     $(obj).children().first().remove();
     $(obj).addClass('playing');
     $(obj).append($(icon));
-
+    $(obj).siblings().find('audio').trigger('load');
     $(obj).siblings().find('audio').trigger('play');
+    $('#footer-play').attr('src','../assets/pause.png');
+    playHandlers.updateFooter(obj);
   },
 
   pauseSong: function(obj){
@@ -30,8 +32,18 @@ var playHandlers = {
     $(obj).children().first().remove();
     $(obj).removeClass('playing');
     $(obj).append($(icon));
-
     $(obj).siblings().find('audio').trigger('pause');
+    $('#footer-play').attr('src','../assets/play_with_circle.png');
+  },
+
+  updateFooter: function(obj){
+
+    // update the artist info
+    var songTitle = $(obj).siblings().find('h3').text();
+    var artistName = $(obj).siblings().find('p').text();
+    $('.footer .song-info h3').text(songTitle);
+    $('.footer .song-info p').text(artistName);
+
   }
 
 };
