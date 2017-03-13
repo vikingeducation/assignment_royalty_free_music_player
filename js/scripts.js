39aac082946e0ab6338ec3ea15e601db3d7ecb25
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-12 21:12:16 -0700

    Finish player controls logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,41 @@ $(document).ready(function() {
 
     var $songContainers = $(".song-container");
     var $allSongs = $("audio");
-    var currentSong = $allSongs[0];
-    var currentSongIndex = 0;
-    var isPlaying = false;
+    var $currentSong = $allSongs.eq(0);
+    var $lastSong = $allSongs.eq(4);
+
+    var $lastPlayButton;
+    var $lastPauseButton;
 
     var progressBar = $(".progress-bar-striped");
     var currentSongTime;
     var duration;
     var currentTimeRatio;
+    var firstRun = true;
 
     $songContainers.click(function(event) {
       var $target = $(this);
       var $playButton = $target.children(".glyphicon-play");
       var $pauseButton = $target.children(".glyphicon-pause");
-      var song = $target.children("audio")[0];
 
-      //if the clicked div is currently playing (so play button is hidden)
-      if ($playButton.hasClass("inactive")) {
-        //Hide the pause button, show the play button (song isn't playing)
-        $playButton.removeClass("inactive");
-        $pauseButton.addClass("inactive");
+      $currentSong = $target.children("audio");
 
-        $allSongs.each(function( index, element ) {
-          this.pause();
-        });
+      //If that song is currently playing
+      if ($playButton.is(":hidden")) {
+        console.log("Pause");
+        $playButton.show();
+        $pauseButton.hide();
+
+        pauseSong($currentSong[0]);
       }
-      //if the clicked div is not currently playing (so pause button is hidden)
+      //Else if that song is not playing
       else {
-        //find any currently playing songs (their play button will be hidden)
-        $(".glyphicon-pause").addClass("inactive");
-        $(".glyphicon-play").removeClass("inactive");
+        console.log("Play");
+        $playButton.hide();
+        $pauseButton.show();
 
-        $playButton.addClass("inactive");
-        $pauseButton.removeClass("inactive");
-
-        $allSongs.each(function( index, element ) {
-          this.pause();
-          isPlaying = false;
-        });
-
-        song.play();
-        isPlaying = true;
+        pauseLastSong();
+        playSong($currentSong[0]);
       }
     });
 
@@ -54,122 +48,95 @@ $(document).ready(function() {
     var $forwardBtn = $(".glyphicon-step-forward");
 
     var stepForward = function () {
-      $allSongs[currentSongIndex].pause();
+      pauseSong();
       resetTrack();
       resetProgress();
 
-      currentSongIndex++;
-      if (currentSongIndex >= $allSongs.length) {
-        currentSongIndex = 0;
-      }
-
-      if (isPlaying) {
-        $allSongs[currentSongIndex].play();
+      if ($allSongs.index($currentSong) <= ($allSongs.length - 2)) {
+        $currentSong = $allSongs.eq($allSongs.index($currentSong) + 1);
+        playSong();
       }
     };
 
     var stepBack = function () {
-      $allSongs[currentSongIndex].pause();
+      pauseSong();
       resetProgress();
 
-      if ($allSongs[currentSongIndex].currentTime < 5) {
-        currentSongIndex--;
+      if ($currentSong[0].currentTime < 5) {
 
-        if (currentSongIndex < 0) {
-          currentSongIndex = $allSongs.length - 1;
+        if ($allSongs.index($currentSong) <= 0) {
+          resetTrack();
+          playSong();
         }
-
-        if (isPlaying) {
-          $allSongs[currentSongIndex].play();
+        else {
+          $currentSong = $allSongs.eq($allSongs.index($currentSong) - 1);
+          playSong();
         }
       }
       else {
         resetTrack();
-        $allSongs[currentSongIndex].play();
+        playSong();
       }
     };
 
-    var play = function() {
-      $footerPlay.addClass("inactive");
-      $footerPause.removeClass("inactive");
-      $allSongs[currentSongIndex].play();
-      isPlaying = true;
-      updateProgress();
+    var playSong = function() {
+      $footerPlay.hide();
+      $footerPause.show();
+      $currentSong.siblings(".glyphicon-pause").show();
+      $currentSong.siblings(".glyphicon-play").hide();
+
+      $currentSong[0].play();
+      $currentSong.addClass("playing");
       $(progressBar).addClass("active");
+      updateProgress();
     };
 
-    var pause = function() {
-      $footerPlay.removeClass("inactive");
-      $footerPause.addClass("inactive");
-      $allSongs[currentSongIndex].pause();
-      isPlaying = false;
+    var pauseSong = function(song) {
+      $footerPlay.show();
+      $footerPause.hide();
 
+      $currentSong.siblings(".glyphicon-play").show();
+      $currentSong.siblings(".glyphicon-pause").hide();
+      $currentSong[0].pause();
+      $currentSong.removeClass("playing");
       $(progressBar).removeClass("active");
+    };
+
+    var updateInfo = function () {
+
+    };
+    
+    var pauseLastSong = function() {
+      var $songToPause = $allSongs.filter(".playing");
+
+
+      if ($songToPause.length > 0) {
+          $songToPause[0].pause();
+          $songToPause[0].currentTime = 0;
+          $songToPause.removeClass("playing");
+          $songToPause.siblings(".glyphicon-pause").hide();
+          $songToPause.siblings(".glyphicon-play").show();
+      }
     };
 
     $backBtn.click(function(event) {
       stepBack();
-      // $allSongs[currentSongIndex].pause();
-      // resetProgress();
-      //
-      // if ($allSongs[currentSongIndex].currentTime < 5) {
-      //   currentSongIndex--;
-      //
-      //   if (currentSongIndex < 0) {
-      //     currentSongIndex = $allSongs.length - 1;
-      //   }
-      //
-      //   if (isPlaying) {
-      //     $allSongs[currentSongIndex].play();
-      //   }
-      // }
-      // else {
-      //   resetTrack();
-      //   $allSongs[currentSongIndex].play();
-      // }
     });
 
     $forwardBtn.click(function(event) {
       stepForward();
-      // $allSongs[currentSongIndex].pause();
-      // resetTrack();
-      // resetProgress();
-      //
-      // currentSongIndex++;
-      // if (currentSongIndex >= $allSongs.length) {
-      //   currentSongIndex = 0;
-      // }
-      //
-      // if (isPlaying) {
-      //   $allSongs[currentSongIndex].play();
-      // }
-
     });
 
     $footerPlay.click(function(event) {
-      play();
-      // $footerPlay.addClass("inactive");
-      // $footerPause.removeClass("inactive");
-      // $allSongs[currentSongIndex].play();
-      // isPlaying = true;
-      // updateProgress();
-      // $(progressBar).addClass("active");
-      //Make matching song's play icon visible, pause icon invisible
+      playSong();
     });
 
     $footerPause.click(function(event) {
-      pause();
-      // $footerPlay.removeClass("inactive");
-      // $footerPause.addClass("inactive");
-      // $allSongs[currentSongIndex].pause();
-      // isPlaying = false;
-      //
-      // $(progressBar).removeClass("active");
-      //Make matching song's play icon visible, pause icon invisible
+      pauseSong();
     });
 
     var resetTrack = function() {
-      $allSongs[currentSongIndex].currentTime = 0;
+      $currentSong[0].currentTime = 0;
     };
 
     $allSongs.on("ended", function(){
@@ -177,20 +144,29 @@ $(document).ready(function() {
     });
 
 //progress bar
-
-
     var resetProgress = function () {
       progressBar.css("width", 0);
     };
 
     var updateProgress = function () {
+      console.log("checking");
       setInterval(function() {
-        if (isPlaying) {
-          currentSongTime = $allSongs[currentSongIndex].currentTime;
-          duration = $allSongs[currentSongIndex].duration;
+        if (isPlaying()) {
+          console.log("Updating");
+          currentSongTime = $currentSong.get(0).currentTime;
+          duration = $currentSong.get(0).duration;
           currentTimeRatio = ((currentSongTime / duration) * 100) + "%";
           progressBar.css("width", currentTimeRatio);
       }
     }, 250);
+
+    var isPlaying = function() {
+      if (!$currentSong.get(0).paused && !$currentSong.get(0).ended && $currentSong.get(0).currentTime > 0) {
+        return true;
+      }
+
+      return false;
+    };
 };
+
   });
