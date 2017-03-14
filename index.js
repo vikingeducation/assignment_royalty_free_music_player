67be9ad04b64b33cc4c94e6454dea571c2621014
tim5046 <tim5046@gmail.com>
2017-03-13 17:16:52 -0700

    wire up play/pause in footer

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
+  pageSetup.init();
   playHandlers.init();
 });
+
+var pageSetup = {
+  init: function(){
+    var firstSong = $('.song-row').first().find('h3').text();
+    var firstArtist = $('.song-row').first().find('p').text();
+    $('.footer .song-info h3').text(firstSong);
+    $('.footer .song-info p').text(firstArtist);
+  }
+};
+
+var songCollection = {
+  list: function(){
+    var songList = {}
+    for( var i=0; i< $('.song-row').length; i++){
+      var songName = $($('.song-row')[i]).find('h3').text();
+      var element = $($('.song-row')[i])
+      songList[songName] = element;
+    }
+    return songList;
+  }
+}
 
 var playHandlers = {
   init: function(){
     $('.play').click(playHandlers.handleClickPlay);
+    $('#footer-previous').click(playHandlers.handleFooterPrevious);
+    $('#footer-play').click(playHandlers.handleFooterPlay);
+    $('#footer-next').click(playHandlers.handleFooterNext);
   },
 
   handleClickPlay: function(){
@@ -14,6 +39,18 @@ var playHandlers = {
       playHandlers.pauseSong($('.playing'));
       playHandlers.playSong($(this));
     }
+  },
+
+  handleFooterPrevious: function(){
+    console.log($(this))
+  },
+  handleFooterPlay: function(){
+    var currentSong = $('.footer .song-info h3').text();
+    var songObject = songCollection.list()[currentSong];
+    $(songObject).children().first().trigger('click');
+  },
+  handleFooterNext: function(){
+    console.log($(this))
   },
 
   playSong: function(obj){
@@ -37,8 +74,7 @@ var playHandlers = {
   },
 
   updateFooter: function(obj){
-
-    // update the artist info
+    // update the artist info in footer
     var songTitle = $(obj).siblings().find('h3').text();
     var artistName = $(obj).siblings().find('p').text();
     $('.footer .song-info h3').text(songTitle);
