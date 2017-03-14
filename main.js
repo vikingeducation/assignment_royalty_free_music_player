f6eab1d9f87d577029642daa3ae51a557ae0a253
Andrew James <ajames20@gmail.com>
2017-03-14 08:36:42 +0200

    bable and comments removed

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
+'use strict';
+
 (function() {
-    const trackList = [{
-            title: 'In it to win it',
-            artist: 'Fleslit',
-            length: '3:22',
-            url: 'tracks/init.mp3'
-        },
-        {
-            title: 'Calm Fight',
-            artist: 'Fleslit',
-            length: '4:24',
-            url: 'tracks/calmfight.mp3'
-        },
-        {
-            title: 'Yummy',
-            artist: 'Fleslit',
-            length: '3:32',
-            url: 'tracks/yummy.mp3'
-        },
-        {
-            title: '12:01 AM',
-            artist: 'Fleslit',
-            length: '3:16',
-            url: 'tracks/1201.mp3'
-        },
-        {
-            title: 'MIA',
-            artist: 'Fleslit',
-            length: '3:14',
-            url: 'tracks/mia.mp3'
-        }
-    ];
-    const playButton = document.getElementById('play-button');
-    const controls = document.querySelectorAll('.control');
-    const musicList = document.querySelector('.music-list');
-    const currentTrack = document.getElementById('current-track');
-    let audio = new Audio(trackList[0].url);
-    let track = [];
-    let title = [];
-    let trackCounter = 0;
-    let titleCounter = 0;
-    // titleCount titleCounterlet musicPlayer = musicPlayer || {};
+    var trackList = [{
+        title: 'In it to win it',
+        artist: 'Fleslit',
+        length: '3:22',
+        url: 'tracks/init.mp3'
+    }, {
+        title: 'Calm Fight',
+        artist: 'Fleslit',
+        length: '4:24',
+        url: 'tracks/calmfight.mp3'
+    }, {
+        title: 'Yummy',
+        artist: 'Fleslit',
+        length: '3:32',
+        url: 'tracks/yummy.mp3'
+    }, {
+        title: '12:01 AM',
+        artist: 'Fleslit',
+        length: '3:16',
+        url: 'tracks/1201.mp3'
+    }, {
+        title: 'MIA',
+        artist: 'Fleslit',
+        length: '3:14',
+        url: 'tracks/mia.mp3'
+    }];
+    var playButton = document.getElementById('play-button');
+    var controls = document.querySelectorAll('.control');
+    var musicList = document.querySelector('.music-list');
+    var currentTrack = document.getElementById('current-track');
+    var audio = new Audio(trackList[0].url);
+    var track = [];
+    var title = [];
+    var trackCounter = 0;
+    var titleCounter = 0;
+
 
     for (var key in trackList) {
-        let li = document.createElement('li');
-        let num = Number(key) + 1;
+        var li = document.createElement('li');
+        var num = Number(key) + 1;
 
-        li.innerHTML = '<p>' + num + '. ' + `${trackList[key].title}` + '</p>' + '<p>' + `${trackList[key].artist}` + '</p>' + '<p>' + `${trackList[key].length}` + '</p>' + '<p><span class="cloud-music">FREE MUSIC ARCHIVE</span></p>';
+        li.innerHTML = '<p>' + num + '. ' + ('' + trackList[key].title) + '</p>' + '<p>' + ('' + trackList[key].artist) + '</p>' + '<p>' + ('' + trackList[key].length) + '</p>' + '<p><span class="cloud-music">FREE MUSIC ARCHIVE</span></p>';
         musicList.appendChild(li);
         track.push(trackList[key].url);
         title.push(trackList[key].title);
         currentTrack.textContent = trackList[0].title;
     }
 
-
     playButton.addEventListener('click', function() {
-        const checkPaused = this.classList.contains('fa-play');
+        var checkPaused = this.classList.contains('fa-play');
 
         if (checkPaused === true) {
             this.classList.add('fa-pause', 'active', 'playing');
@@ -80,11 +76,10 @@
         audio.pause();
     }
 
-
     controls.forEach(function(controls) {
         return controls.addEventListener('click', function() {
-            const forward = this.classList.contains('fa-step-forward');
-            const backward = this.classList.contains('fa-step-backward');
+            var forward = this.classList.contains('fa-step-forward');
+            var backward = this.classList.contains('fa-step-backward');
 
             if (forward) {
                 currentTrack.textContent = nextItem();
@@ -99,28 +94,28 @@
                 audio = new Audio(nextSong());
                 playAudio();
             }
-
         });
 
         function nextItem() {
-            titleCounter = titleCounter + 1; // increase i by one
-            titleCounter = titleCounter % title.length; // if we've gone too high, start from `0` again
+            titleCounter = titleCounter + 1;
+            titleCounter = titleCounter % title.length;
 
-            return title[titleCounter]; // give us back the item of where we are now
+            return title[titleCounter];
         }
 
         function nextSong() {
-            trackCounter = trackCounter + 1; // increase i by one
-            trackCounter = trackCounter % track.length; // if we've gone too high, start from `0` again
-            return track[trackCounter]; // give us back the item of where we are now
+            trackCounter = trackCounter + 1;
+            trackCounter = trackCounter % track.length;
+            return track[trackCounter];
         }
 
         function prevItem() {
-            if (titleCounter === 0) { // i would become 0
-                titleCounter = title.length; // so put it at the other end of the array
+            if (titleCounter === 0) {
+
+                titleCounter = title.length;
             }
-            titleCounter = titleCounter - 1; // decrease by one
-            return title[titleCounter]; // give us back the item of where we are now
+            titleCounter = titleCounter - 1;
+            return title[titleCounter];
         }
     });
 })();
