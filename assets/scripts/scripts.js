0cd81d9a9440144b09cd77bb0ced58d58dccb9f8
Christian Florez <florez.christian@gmail.com>
2017-04-13 21:25:04 -0400

    add prev next btns jquery

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -11,22 +11,55 @@
                 $audioMainBtn: $('.audio-controls-main-button'),
                 $audioNextBtn: $('.audio-controls-next-button'),
                 $audioCurrentTitle: $('.currently-playing h2'),
-                $audioCurrentArtist: $('.currently-playing h3')
+                $audioCurrentArtist: $('.currently-playing h3'),
+                $currentlyPlayingInList: $('.currently-playing-list')
             };
             rfmPlayer.setup();
         },
 
         setup: function() {
             rfmPlayer.enableTrackPlay();
+            rfmPlayer.enableNextBtn();
+            rfmPlayer.enablePrevBtn();
         },
 
         enableTrackPlay: function() {
-            rfmPlayer.config.$trackControlBtns.on("mouseup.trackControlBtns", function() {
+            rfmPlayer.config.$trackControlBtns.on("click.trackControlBtns", function() {
                 $(this).toggleClass("track-play-button");
                 $(this).toggleClass("track-pause-button");
                 console.log('You clicked a play/pause button. Good job!');
             });
+        },
+
+        enableNextBtn: function() {
+            rfmPlayer.config.$audioNextBtn.on("click.next", function() {
+                var $current = rfmPlayer.config.$currentlyPlayingInList;
+                var $next = $current.closest('li')
+                    .next('li');
+                if ($next.length === 0) {
+                    $next = $current;
+                }
+                $current.toggleClass('currently-playing-list');
+                $next.toggleClass('currently-playing-list');
+                rfmPlayer.config.$currentlyPlayingInList = $next;
+            });
+        },
+
+        enablePrevBtn: function() {
+            rfmPlayer.config.$audioPrevBtn.on("click.prev", function() {
+                var $current = rfmPlayer.config.$currentlyPlayingInList;
+                var $prev = $current.closest('li')
+                    .prev('li');
+                if ($prev.length === 0) {
+                    $prev = $current;
+                }
+                $current.toggleClass('currently-playing-list');
+                $prev.toggleClass('currently-playing-list');
+                rfmPlayer.config.$currentlyPlayingInList = $prev;
+            });
         }
+
+
     };
 
     $(document).ready(function() {
