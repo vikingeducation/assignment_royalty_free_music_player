a1213ebf8cd42bab166b83b1b5f3f5f2a21ac241
Kara Thrash <dvdonato@gmail.com>
2017-06-20 16:21:44 -0400

    music plays, tracks change

--- scripts.js
+++ scripts.js
@@ -1,59 +1,73 @@
 // FILL IN THE FUNCTIONS BELOW
-maxFieldChar = 32;
-currentFieldChar = 0;
-maxAreaChar = 140;
-currentAreaChar = 0;
-maxPasswordChar = 16;
-currentPasswordChar = 0;
+currentTrackAudio = "Lupe Fiasco - Kick Push.mp3";
+currentTrackText = "Lupe Fiasco - Kick Push";
+lastTrackText = "";
+musicPlaying = false;
+songs = ["Lupe Fiasco - Kick Push","Suicide - Ghost Rider","Janelle Monae feat. Miguel - Primetime"]
+trackNumber = 0
 
-
- var CountFieldChar = function(){
-   document.getElementById("fieldCounterText").style.color = "black";
-var charCount = document.getElementById("field").value.length;
-
-if (charCount <= 0) { document.getElementById("fieldCounterText").innerHTML = " ";}
-else{document.getElementById("fieldCounterText").innerHTML = charCount;}
- }
-
- var CountAreaChar = function(){
-   document.getElementById("areaCounterText").style.color = "black";
-var charCount = document.getElementById("area").value.length;
-
-if (charCount <= 0) { document.getElementById("areaCounterText").innerHTML = " ";}
-else{document.getElementById("areaCounterText").innerHTML = charCount;}
- }
- var CountPasswordChar = function(){
-var charCount = document.getElementById("pass").value.length;
-
-if (charCount <= 0) { document.getElementById("validate").innerHTML = ""; document.getElementById("passCounterText").innerHTML = " ";}
-else{document.getElementById("passCounterText").innerHTML = charCount;
-CheckPassword();
+var SongOver = function()
+{
+document.getElementById("MainPlayButton").src="PlayButton.png";
 }
- }
-
-var CheckPassword =  function(){
-  if(document.getElementById("pass").value === "word")
-  {
-    document.getElementById("validate").style.color = "black";
-document.getElementById("validate").innerHTML = "log in";
-  }else{
-  document.getElementById("validate").style.color = "red";
-    document.getElementById("validate").innerHTML = "password not right";}
+var FindSongIndex = function(songSelected)
+{
 
 }
-var ValidateLogIn =  function(){
-if(document.getElementById("field").value.length > 32){
-document.getElementById("fieldCounterText").innerHTML = "Too Long";
-document.getElementById("fieldCounterText").style.color = "red";
- }
- if(document.getElementById("area").value.length > 16){
- document.getElementById("areaCounterText").innerHTML = "Too Long";
- document.getElementById("areaCounterText").style.color = "red";
-  }
+var TrackSkip = function(fwdOrBack)
+{
+trackNumber += fwdOrBack;
+if(trackNumber > songs.length - 1){trackNumber = 0;}
+if(trackNumber < 0){trackNumber = songs.length - 1;}
+lastTrackText = currentTrackText;
+document.getElementById("LastPlayed").textContent = lastTrackText;
+currentTrackAudio = songs[trackNumber] + ".mp3";
+currentTrackText = songs[trackNumber];
+document.getElementById("currentTrackText").textContent = currentTrackText;
+audioPlayer = document.getElementById("currentTrackAudio");
+audioPlayer.src = currentTrackAudio;
+musicPlaying = true;
+audioPlayer.play();
+}
 
-  if(document.getElementById("pass").value === "word")
+var PlayThisTrack =  function(thisTrack){
+  if(thisTrack.textContent != currentTrackText)
   {
+currentTrackAudio = thisTrack.textContent + ".mp3";
+
+audioPlayer = document.getElementById("currentTrackAudio");
+audioPlayer.src = currentTrackAudio;
+
+lastTrackText = currentTrackText;
 
 
-  }
+currentTrackText = thisTrack.textContent;
+document.getElementById("currentTrackText").textContent = currentTrackText;
+trackNumber = songs.indexOf(currentTrackText);
+document.getElementById("LastPlayed").textContent = lastTrackText;
+
+if(musicPlaying === true){
+
+audioPlayer.play();
+}
+else{
+audioPlayer.pause();
+}
+}
+}
+
+
+var PlayToggle =  function(){
+  audioPlayer = document.getElementById("currentTrackAudio");
+
+if(musicPlaying === true){musicPlaying = false;
+audioPlayer.pause();
+document.getElementById("MainPlayButton").src="PlayButton.png";
+}
+else{musicPlaying = true;
+audioPlayer.play();
+document.getElementById("MainPlayButton").src="PauseButton.png";
+}
+
+
 }
