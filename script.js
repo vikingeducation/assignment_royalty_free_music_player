53ce87687aa4a2b7d558c2682f0f4b7e70f81990
Eric Glover <etgt28@mail.missouri.edu>
2017-07-10 05:39:31 -0500

    all functionality complete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   player = new Player();
   player.play(4);
   player.setup_click_listeners();
+  player.toggle_play_bar();
 });
 
 
@@ -13,7 +14,7 @@ Player = function() {
     'assets/music/Henry_Homesweet/Henry_Homesweet_-_04_-_Fifteen.mp3',
     'assets/music/Henry_Homesweet/Henry_Homesweet_-_05_-_Garble_House.mp3',
     'assets/music/Henry_Homesweet/Henry_Homesweet_-_06_-_Lulla_Bye.mp3',
-    'assets/music/Henry_Homesweet/Henry_Homesweet_-_07_-_Micro_Love.mp3',      //getting an error with this one
+    //'assets/music/Henry_Homesweet/Henry_Homesweet_-_07_-_Micro_Love.mp3',      //getting an error with this one
     'assets/music/Henry_Homesweet/Henry_Homesweet_-_08_-_Okiirobo_Navigation_System.mp3',
     'assets/music/Henry_Homesweet/Henry_Homesweet_-_09_-_Satori.mp3',
     'assets/music/Henry_Homesweet/Henry_Homesweet_-_10_-_Sprite_Heart_Bleeds_No_More.mp3',
@@ -25,7 +26,7 @@ Player = function() {
     'Fifteen',
     'Garble House',
     'Lulla Bye',
-    'Micro Love',
+    //'Micro Love',    //getting an error with this one
     'Okiirobo Navigation System',
     'Satori',
     'Sprite Heart Bleeds No More',
@@ -103,7 +104,7 @@ Player = function() {
     var _this = window.player;
     console.log(_this)
     var index = _this.get_track_index( _this.now_playing ) + 1
-    if ( _this.tracks.length < index ){
+    if ( _this.tracks.length - 1 < index ){
       index = 0;
     }
     _this.play( index );
@@ -117,47 +118,78 @@ Player = function() {
     }
     _this.play( index );
   }
+  this.toggle_play_bar = function() {
+    var $play_button = $("#play").children();
 
-  //start a specific song
+    if ( this.now_playing.paused ){
+      //music was unpaused
+      $play_button.removeClass('glyphicon-pause');
+      $play_button.addClass("glyphicon-play-circle");
+
+    }else {
+      $play_button.removeClass('glyphicon-play-circle');
+      $play_button.addClass("glyphicon-pause");
+    }
+  }
+  this.toggle_track = function( index ) {
+    //if paused
+    if ( this.tracks[ index ]['audio'].paused ){
+      this.$tracks[ index ].children("button").children().removeClass("glyphicon-pause");
+      this.$tracks[ index ].children("button").children().addClass("glyphicon-play");
+    }else {
+      this.$tracks[ index ].children("button").children().removeClass("glyphicon-play");
+      this.$tracks[ index ].children("button").children().addClass("glyphicon-pause");
+    }
+  }
+
+  //start a different song
   this.play = function(index){
     var _this = window.player;
 
     //stop the last track
     this.now_playing.pause()
     player.now_playing.currentTime = 0;
-
-    //change the buttons
+    this.toggle_track( this.get_track_index(this.now_playing) );
 
     //update the play-bar
     var $track_wrapper = _this.$tracks[index]
     var title_string = $track_wrapper.children().children().filter(".track-title").text();
     var artist_string = $track_wrapper.children().children().filter(".track-artist").text();
-    //$("#play-bar .track-title").text( _this.$tracks[index].children().filter(".track-title").text() )
+
 
     $("#play-bar .track-title").text( title_string );
     $("#play-bar .track-artist").text( artist_string );
 
 
-    console.log(index)
+    //console.log(index)
     _this.tracks[index]['audio'].play();
     _this.now_playing = _this.tracks[index]['audio'];
     _this.now_playing.addEventListener('ended', _this.next, "once")
 
+    //change the buttons
+    this.toggle_track( index );
+    this.toggle_play_bar();
+
+
   }
   //pause song
-  this.pause = function(){
+  this.pause = function( index ){
     var _this = window.player;
     _this.now_playing.pause();
 
     //change buttons
+    this.toggle_track( this.get_track_index( this.now_playing ) );
+    this.toggle_play_bar();
 
   }
   //resume playing
-  this.resume = function(){
+  this.resume = function( index ){
     var _this = window.player;
     _this.now_playing.play();
 
     //change buttons
+    this.toggle_track( this.get_track_index( this.now_playing ) );
+    this.toggle_play_bar();
 
   }
   this.get_track_index = function( song ) {
@@ -181,13 +213,31 @@ Player = function() {
             //_this.play( $track )
         })
     })
+    $("#last").on("click", function(){
+      window.player.previous();
+    })
+    $("#next").on("click", function(){
+      window.player.next();
+    })
+    $("#play").on("click", function(){
+      window.player.play_button_clicked();
+    })
+  }
+  this.play_button_clicked = function(){
+    if ( this.now_playing.paused ){
+      this.resume();
+    }else{
+      this.pause();
+    }
+  //  this.toggle_track( this.get_track_index(this.now_playing) );
+    //this.toggle_play_bar();
   }
 
   //the user clicked a track
   this.clicked = function( e, $track ){
-    console.log( e );
-    console.log( $track );
-    console.log( this );
+    //console.log( e );
+    //console.log( $track );
+    //console.log( this );
 
     //match the track
     var index = 0;
@@ -199,11 +249,20 @@ Player = function() {
     }
 
     //find out if that track is playing
-    if ( this.now_playing == this.tracks[i]['audio'] ){
-      
+    if ( this.now_playing == this.tracks[ index ]['audio'] ){
+      //it's either paused or playing
+      if ( this.now_playing.paused ){
+        //resume it
+        this.resume( index );
+      }else {
+        //pause it
+        this.pause( index );
+      }
+    }else {
+      //play the track
+      this.play( index );
     }
-    //play the track
-    this.play( index );
+
   }
 
 
