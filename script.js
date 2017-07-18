3ccc6bf34d38d58ca06a9a23e3aa5b1290fa7110
Timi <oblacishere@gmail.com>
2017-07-18 15:47:14 +0100

    rounding up

--- script.js
+++ script.js
@@ -1,74 +1,71 @@
 $(function () {
 	"use strict";
-/*
-    	     function controlMusic() {
-             var playlist =  new Array ('audio/0.mp3', 'audio/1.mp3', 'audio/2.mp3', 'audio/3.mp3', 'audio/4.mp3');
-           	  if (playing) {
-           	   // Stop playing
-		        song.pause();
-		      } else {
-		          // Start playing
-		          if (!initDone) {	
-		             initDone = true;    	      
-    	              song = document.getElementById('play');
-  		              song.addEventListener('ended', function() {
-		              song.play();
-		            });
-		        }
+	var playlist = ['audio/0.mp3', 'audio/1.mp3', 'audio/2.mp3', 'audio/3.mp3', 'audio/4.mp3'];
+	 var song = new Audio();
+		song.preload = 'auto';
+		song.addEventListener('ended', function() {
+			var i = Math.floor(Math.random() * 4);
+			$('#current').html($(i).html());
+			console.log(i);
+			song.src = playlist[i];
+			song.play();
+		}, true);
+		song.loop = false;
+    
+		/*alert(identifier);*/
 
-
-		    song.play();
-		  }
-
-		  playing = !playing;
-		}
-		
-		var playing = false;
-		var initDone = false;
-		var song = null;
-	//	song.preload = 'auto';
-		var chooser = "";
-		function play() {
-		
-				song.play();
-		}
-
-		function pause() {
-
-			song.pause();
-		} */
-    	var song = document.getElementById('play');
-		function play() {
-    		if (song.paused) {
-    			song.play();
-    		}else {
-    			song.play();
-    			song.currentTime = 0;
-    		}
+    var nextSong = null;
+    var prevSong = null;
+	function play() {
+    	if (song.paused) {
+    		song.play();
+    		song.currentTime = 0;
+    	} else {
+    		song.play();
+    		song.currentTime = 0;
     	}
+    }
 
-    	function toggleControl() {
-    		if (song.paused) {
-    			song.play();
-    		}else {
-    			song.pause();
-    		}	
-    	}
 
+    //CONTROLS
+
+    function pause() {
+    	song.pause();
+    	song.currentTime = 0;
+    }
+
+    function toggleControl() {
+    	if (song.paused) {
+    		song.play();
+    	}else {
+    		song.pause();
+    	}	
+    }
+
+	//CLICK EVENT & TOGGLE ICONS
 	$('.song').click( function() {
-		 play();
-    	  $('#current').html($(this).html());
-    	    $('.fa-play-circle-o').addClass( 'fa-pause-circle-o');
-    	});
+		var identifier = $(this).attr('data-panelid');
+		song.src = playlist[identifier];
+		play();
+		$('#current').html($(this).html());
+    	$('.fa-play-circle-o').addClass( 'fa-pause-circle-o');
+    });
 
 	$('.fa-play-circle-o').click( function() {
-		$('.fa-play-circle-o').toggleClass( 'fa-pause-circle-o');
-		toggleControl();
+		if( song.src == "" ) {
+			alert('select a song first!');
+		} else {
+			$('.fa-play-circle-o').toggleClass( 'fa-pause-circle-o');
+			toggleControl();
+		}
 	});
 
 	$('.fa-step-backward').click(function() {
 		$('.current').html($(this).html());
 		$('.fa-play-circle-o').addClass( 'fa-pause-circle-o');
+		song.src = playlist[identifier - 1];
+		pause();
+		play();
 	});
 
 	$('.fa-step-forward').click(function() {
@@ -77,13 +74,15 @@ $(function () {
 	});
 
 
+	//HOVER EFFECTS 
 	$('.song').mousemove( function() {
-		$(this).css('background-color', '#97A7B3');
+		$(this)
+		.css('font-size', '18px');
 	});
 
 
 	$('.song').mouseout( function() {
-		$(this).css('background-color', 'transparent');
+		$(this).css('font-size', '16px');
 	});
 	
 });
