16b032c27044601d83814b560904b7a09b7ee57e
anne <richardson.ae@gmail.com>
2017-07-27 09:43:18 -0500

    Create play/pause function on songs in list

--- scripts.js
+++ scripts.js
@@ -1,8 +1,44 @@
 "use strict";
 $( document ).ready(function() {
 
+  $('.song-list ul li').on('click', 'i.play', function(e){
+    $(this).parent().find('.song-details audio').trigger('load');
+    $(this).parent().find('.song-details audio').trigger('play');
+    $(this).parent().addClass('active');
+    $(this).text('pause')
+            .removeClass('play')
+            .addClass('pause');
+  });
+
+  $('.song-list ul li').on('click', 'i.pause', function(e){
+    $(this).parent().find('.song-details audio').trigger('pause');
+    $(this).parent().removeClass('active');
+    $(this).text('play_arrow')
+            .removeClass('pause')
+            .addClass('play');
+  });
 
 
+
+
+    // $(this).parent().find('.song-details p').first().css('border', '1px solid blue');
+
+    // console.log(
+    //   $(this).parent().find('.song-details audio source').attr('src')
+    //   );
+
+
+
+
+
+  // //starts playing
+  // $(".audioDemo").trigger('play');
+  // //pause playing
+  // $(".audioDemo").trigger('pause');
+
+  // $('header').on('click', 'h1', function(e){
+  //   console.log($(this).text());
+  // });
 
 
 
