6f7f1995141011e8d81500ecca25c463c0a14ad7
anne <richardson.ae@gmail.com>
2017-07-27 14:48:34 -0500

    Refactor so users can click on the li and not just the play/pause icon

--- scripts.js
+++ scripts.js
@@ -2,37 +2,61 @@
 $( document ).ready(function() {
 
 
+// -----------sandbox-------------------------------------------
 
+  // document.getElementById('bg_audio').play();
+
+  // $('#playButton').on('click', function(e){
+  //   $('#bg_audio').trigger("play");
+  // });
+
+  // $('#pauseButton').on('click', function(e){
+  //   $('#bg_audio').trigger("pause");
+  // });
+
+
+  // document.getElementById('bg_audio').addEventListener('ended', function(){
+  //     $('#jschanger').css('border', '1px solid blue');
+  // });
+
+  // $('#bg_audio').on('ended', function(){
+  //     $('#jqchanger').css('border', '1px solid red');
+  // });
+
+// ------------------------------------------------------
 
   // Play the song when the play button is clicked.
-  $('.song-list ul li').on('click', 'i.play', function(e){
-    var $currentSong = $(this).parent().find('.song-details audio');
+  $('.song-list ul').on('click', 'li', function(e){
+    var $currentSong = $(this).find('.song-details audio');
 
     $currentSong.trigger('play');
-    $(this).parent().addClass('active');
-    $(this).text('pause')
+    $(this).addClass('active')
            .removeClass('play')
            .addClass('pause');
+    $(this).find('i').text('pause');
 
     // When the song is over, reset the styles back to the 'play' state
     $currentSong.on('ended', function(){
-      $(this).closest('li').removeClass('active');
-      $(this).closest('li').find('i')
-             .text('play_arrow')
+      $(this).closest('li')
+             .removeClass('active')
              .removeClass('pause')
              .addClass('play');
+      $(this).closest('li').find('i')
+             .text('play_arrow');
     });
   });
 
   // Pause the song when the pause button is clicked
-  $('.song-list ul li').on('click', 'i.pause', function(e){
-    var $currentSong = $(this).parent().find('.song-details audio');
+  $('.song-list ul').on('click', 'li.pause', function(e){
+    var $currentSong = $(this).find('.song-details audio');
 
     $currentSong.trigger('pause');
-    $(this).parent().removeClass('active');
-    $(this).text('play_arrow')
+    $(this).closest('li')
+           .removeClass('active')
            .removeClass('pause')
            .addClass('play');
+    $(this).closest('li').find('i')
+           .text('play_arrow');
   });
 
 
