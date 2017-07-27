5cac1f3e55d2e047c5e59b7a48b5a06e75a6a9e1
anne <richardson.ae@gmail.com>
2017-07-27 14:11:53 -0500

    Refactor 'current song' into vars

--- scripts.js
+++ scripts.js
@@ -1,47 +1,32 @@
 "use strict";
 $( document ).ready(function() {
 
+
+
+
+  // Play the song when the play button is clicked.
   $('.song-list ul li').on('click', 'i.play', function(e){
-    $(this).parent().find('.song-details audio').trigger('load');
-    $(this).parent().find('.song-details audio').trigger('play');
+    var $currentSong = $(this).parent().find('.song-details audio');
+
+    $currentSong.trigger('play');
     $(this).parent().addClass('active');
     $(this).text('pause')
-            .removeClass('play')
-            .addClass('pause');
+           .removeClass('play')
+           .addClass('pause');
+
+
   });
 
+  // Pause the song when the pause button is clicked
   $('.song-list ul li').on('click', 'i.pause', function(e){
-    $(this).parent().find('.song-details audio').trigger('pause');
+    var $currentSong = $(this).parent().find('.song-details audio');
+
+    $currentSong.trigger('pause');
     $(this).parent().removeClass('active');
     $(this).text('play_arrow')
-            .removeClass('pause')
-            .addClass('play');
+           .removeClass('pause')
+           .addClass('play');
   });
-
-
-
-
-    // $(this).parent().find('.song-details p').first().css('border', '1px solid blue');
-
-    // console.log(
-    //   $(this).parent().find('.song-details audio source').attr('src')
-    //   );
-
-
-
-
-
-  // //starts playing
-  // $(".audioDemo").trigger('play');
-  // //pause playing
-  // $(".audioDemo").trigger('pause');
-
-  // $('header').on('click', 'h1', function(e){
-  //   console.log($(this).text());
-  // });
-
-
-
 
 
 });
