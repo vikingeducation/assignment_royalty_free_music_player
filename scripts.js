a587bd3cf6d3adb9f5e8ed6549c800b21123a56c
anne <richardson.ae@gmail.com>
2017-07-28 15:29:50 -0500

    Refactor current song logic to come from the Player class instead of a jQuery listener

--- scripts.js
+++ scripts.js
@@ -58,8 +58,8 @@ $( document ).ready(function() {
       this.songs = songSet;
     };
     this.currentSong = {};
-    this.set_currentSong = function(id) {
-      this.currentSong = this.findSong(id);
+    this.setCurrentSong = function(id) {
+      this.currentSong = this.songs[id];
       //this.renderSong ?
       return true;
     };
@@ -88,6 +88,7 @@ $( document ).ready(function() {
               .attr('type', 'audio/ogg');
 
     var $songAudioSection = $('<audio></audio>')
+              .attr('id', song.id)
               .attr('preload','none')
               .text('Your browser does not support HTML5 audio.')
               .prepend($songOgg)
@@ -113,16 +114,17 @@ $( document ).ready(function() {
   });
 
 
-  // Play the song when the play button is clicked.
+  // Play the song when the li is clicked.
   $('.song-list ul').on('click', 'li', function(e){
-    var $currentSong = $(this).find('.song-details audio');
+    var $clickedSong = $(this).find('.song-details audio');
+    player.setCurrentSong($clickedSong.get(0).id);
 
-    $currentSong.trigger('play');
+    $clickedSong.trigger('play');
     $(this).addClass('active');
     $(this).find('i').text('pause');
 
     // When the song is over, reset the styles back to the 'play' state
-    $currentSong.on('ended', function(){
+    $clickedSong.on('ended', function(){
       $(this).closest('li')
              .removeClass('active');
       $(this).closest('li').find('i')
@@ -130,29 +132,25 @@ $( document ).ready(function() {
     });
   });
 
-  // Pause the song when the pause button is clicked
+  // Pause the song when an active li is clicked
   $('.song-list ul').on('click', 'li.active', function(e){
-    var $currentSong = $(this).find('.song-details audio');
+    var $clickedSong = $(this).find('.song-details audio');
 
-    $currentSong.trigger('pause');
+    $clickedSong.trigger('pause');
     $(this).closest('li')
            .removeClass('active');
     $(this).closest('li').find('i')
            .text('play_arrow');
   });
 
-  // Put the current song details in the player on the bottom
+  // Put the clicked song details in the player on the bottom
   $('.container').on('click', '.song-list ul li.active', function(e){
     console.log('clicked a song thats not playing');
 
-    var $currentSongDetails = $(this).find('.song-details');
-    var $currentTitle = $currentSongDetails.find('.title').text();
-    var $currentArtist = $currentSongDetails.find('.artist').text();
-    var $currentSong = $currentSongDetails.find('audio');
-
+    var $currentSong = player.currentSong;
     var $playerSongDetails = $('section.player .song-details')
-    $playerSongDetails.find('.title').text($currentTitle);
-    $playerSongDetails.find('.artist').text($currentArtist);
+    $playerSongDetails.find('.title').text($currentSong.title);
+    $playerSongDetails.find('.artist').text($currentSong.artist);
 
 
     // Change the Player play button to a pause button
