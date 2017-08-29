a1cd6b533a06280a6c3660f822f72f209d82d7c4
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-08-29 09:14:37 +0200

    finished music player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,87 @@
-var tracks = [{
-  title: "Subzero",
-  artist: "Ben Klock",
-  url: "http://www36.zippyshare.com/d/WTpaDpHX/17920/Ben%20Klock%20-%20Subzero%20%28Original%20Mix%29.mp3"
-},{
-  title: "Higher Level (Bicep Remix)",
-  artist: "Isaac Tichauer",
-  url: "http://www120.zippyshare.com/d/lT7ZgFOf/21209/Isaac%20Tichauer%20-%20Higher%20Level%20%28Bicep%20Remix%29%20%5bClapCrate.com%5d.mp3"
-},{
-  title: "Sometimes (Original Mix)",
-  artist: "Detroit Swindle",
-  url: "http://www26.zippyshare.com/d/P9ujfI1o/18924/Detroit%20Swindle%20-%20Sometimes%20%28Original%20Mix%29.mp3"
-},{
-  title: "B.F.O.D.A.A.S",
-  artist: "Mall Grab",
-  url: "http://www117.zippyshare.com/d/i31FJ7se/51551/Mall%20Grab%20-%20B.F.O.D.A.A.S%20%5bClapCrate.net%5d.mp3"
-},{
-  title: "German Winter (Original Mix)",
-  artist: "andhim",
-  url: "http://www44.zippyshare.com/d/JVnLbj2l/21799/Andhim%20-%20German%20Winter%20%28Original%20Mix%29.mp3"
-}];
-
 $(document).ready(function() {
 
-  $(".play-btn").on("click", function(event) {
-    event.preventDefault();
-    $target = $(this);
+  $("audio").trigger("load");
+
+  $(".play-pause").on("click", function() {
+    let $target = $(this);
+    let $audio = $target.parent().next("audio");
     loadTrack($target);
-    updateControls($target);
-    // playSong();
+
+    if ($target.hasClass("active") && $target.hasClass("glyphicon-play")) {
+      $target.removeClass("glyphicon-play").addClass("glyphicon-pause");
+      $(".footer-play-pause").removeClass("glyphicon-play").addClass("glyphicon-pause");
+      $audio.trigger("play");
+    } else if ($target.hasClass("active") && $target.hasClass("glyphicon-pause")) {
+      $target.removeClass("glyphicon-pause").addClass("glyphicon-play");
+      $(".footer-play-pause").removeClass("glyphicon-pause").addClass("glyphicon-play");
+      $audio.trigger("pause");
+    } else {
+      toggleOtherTrack($target, $audio);
+    };
   });
+
+  $(".footer-play-pause").on("click", function() {
+    let $target = $(this);
+    let $audio = $(".active").parent().next("audio");
+
+    if ($target.hasClass("glyphicon-play")) {
+      $target.removeClass("glyphicon-play").addClass("glyphicon-pause");
+      $(".active").removeClass("glyphicon-play").addClass("glyphicon-pause");
+      $audio.trigger("play");
+    } else {
+      $target.removeClass("glyphicon-pause").addClass("glyphicon-play");
+      $(".active").removeClass("glyphicon-pause").addClass("glyphicon-play");
+      $audio.trigger("pause");
+    };
+  });
+
+  $(".footer-previous").on("click", function() {
+    let $currentIndex = $(".play-pause").index($(".active"));
+    let $target;
+    let $audio;
+
+    if ($currentIndex === 0) {
+      $target = $(".play-pause").last();
+      $audio = $target.parent().next("audio");
+    } else {
+      $target = ($(".play-pause").eq($currentIndex - 1));
+      $audio = $target.parent().next("audio");
+    };
+
+    toggleOtherTrack($target, $audio);
+  });
+
+  $(".footer-next").on("click", function() {
+    let $currentIndex = $(".play-pause").index($(".active"));
+    let $target;
+    let $audio;
+
+    if ($currentIndex === $(".play-pause").length - 1) {
+      $target = $(".play-pause").first();
+      $audio = $target.parent().next("audio");
+    } else {
+      $target = ($(".play-pause").eq($currentIndex + 1));
+      $audio = $target.parent().next("audio");
+    };
+
+    toggleOtherTrack($target, $audio);
+  });
+
+  loadTrack = function($target) {
+    let title = $target.parent().parent().next().find(".track-title").text();
+    let artist = $target.parent().parent().next().find(".artist").text();
+
+    $(".track-selected").text(title);
+    $(".artist-selected").text(artist);
+  };
+
+  toggleOtherTrack = function($target, $audio) {
+    $(".play-pause").removeClass("active").removeClass("glyphicon-pause").addClass("glyphicon-play");
+    $("audio").trigger("pause").prop("currentTime", 0);
+    $target.addClass("active").removeClass("glyphicon-play").addClass("glyphicon-pause");
+    loadTrack($target);
+    $(".footer-play-pause").removeClass("glyphicon-play").addClass("glyphicon-pause");
+    $audio.trigger("play");
+  };
+
 });
-
-loadTrack = function($target) {
-  var title = $target.parent().next().find(".track-title").text();
-  var artist = $target.parent().next().find(".artist").text();
-  var url = (tracks.find(u => u.title === title)).url;
-
-  $(".track-selected").find(".track-title")
-    .text(title);
-  $(".track-selected").find(".artist")
-    .text(artist);
-  $("audio source").attr("src", url);
-};
-
-updateControls = function($target) {
-  $(".glyphicon-pause")
-    .removeClass("glyphicon-pause")
-    .addClass("glyphicon-play");
-  $(".navbar .play-btn").find("span")
-    .removeClass("glyphicon-play")
-    .addClass("glyphicon-pause");
-  $target.find("span")
-    .removeClass("glyphicon-play")
-    .addClass("glyphicon-pause");
-};
