7563502344f15e96493b16ee3ad42bb358699439
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-09-21 18:45:29 -0400

    songs populate into player

--- scripts.js
+++ scripts.js
@@ -1,36 +1,108 @@
 'use strict';
 
-//create song list & populate songs into player
+//create song list
 
-//when song/play is clicked
-	//icon turns to pause
-	//song plays
-	//song populates into footer
-	//song name in list changes color
+const SongList = {
+	song1: {
+		artist: "bensound",
+		title: "A New Beginning",
+		url: "/music/bensound-anewbeginning.mp3"
+	},
+	song2: {
+		artist: "bensound",
+		title: "Energy",
+		url: "/music/bensound-energy.mp3"
+	},
+	song3: {
+		artist: "bensound",
+		title: "Going Higher",
+		url: "/music/bensound-goinghigher.mp3"
+	},
+	song4: {
+		artist: "bensound",
+		title: "Happy Rock",
+		url: "/music/bensound-happyrock.mp3"
+	},
+	song5: {
+		artist: "bensound",
+		title: "Jazzy Frenchy",
+		url: "/music/bensound-jazzyfrenchy.mp3"
+	},
+	song6: {
+		artist: "bensound",
+		title: "Ukulele",
+		url: "/music/bensound-uklulele.mp3"
+	}
+}
 
-//when song/pause is clicked
-	//icon turns to play
-	//song pauses
-	//song stays in footer
-	//song name in list changes color
+//create player object
 
-//when next button is clicked
-	//next song is selected & populates into footer
-	//next song plays
-	//song name in list changes color
-	//if at last song, go to first
+let player = {
 
-//when previous button is clicked
-	//previous song is selected & populates into footer
-	//previous song plays
-	//song name in list changes color
-	//if at first song, go to last
+	//declare player variables
+	$songList: $('.song-list'),
+
+	//loop through all songs in songList & populate eac one into player
+	populateSong: function(songs) {
+			
+		let songNumber = 1
+		
+		for (var song in songs) {
+			this.$songList
+				.append('<li class="song song-' + songNumber + '">\
+				<div class="list-play-icon">\
+					<i class="fa fa-play" aria-hidden="true"></i>\
+				</div>\
+				<div class="list-pause-icon">\
+					<i class="fa fa-pause" aria-hidden="true"></i>\
+				</div>\
+				<div class="song-info">\
+					<h5 class="artist">' + (songs[song]).artist + '</h5>\
+					<p class="title">' + (songs[song]).title + '</p>\
+				</div>\
+			</li>');
+				
+
+			songNumber += 1;
+		}
+
+	} //populateSongs
 
 
 
 
 
 
+	//when song/play is clicked
+		//icon turns to pause
+		//song plays
+		//song populates into footer
+		//song name in list changes color
+
+	//when song/pause is clicked
+		//icon turns to play
+		//song pauses
+		//song stays in footer
+		//song name in list changes color
+
+	//when next button is clicked
+		//next song is selected & populates into footer
+		//next song plays
+		//song name in list changes color
+		//if at last song, go to first
+
+	//when previous button is clicked
+		//previous song is selected & populates into footer
+		//previous song plays
+		//song name in list changes color
+		//if at first song, go to last
+
+
+
+} //player
+
+
+player.populateSong(SongList);
 
 
 
