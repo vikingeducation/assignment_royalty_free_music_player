63a03f7002f73db74056de08919288834d563364
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-09-21 15:33:32 -0400

    songs added to folder & scripts outline commented

---
+++ scripts.js
@@ -0,0 +1,43 @@
+'use strict';
+
+//create song list & populate songs into player
+
+//when song/play is clicked
+	//icon turns to pause
+	//song plays
+	//song populates into footer
+	//song name in list changes color
+
+//when song/pause is clicked
+	//icon turns to play
+	//song pauses
+	//song stays in footer
+	//song name in list changes color
+
+//when next button is clicked
+	//next song is selected & populates into footer
+	//next song plays
+	//song name in list changes color
+	//if at last song, go to first
+
+//when previous button is clicked
+	//previous song is selected & populates into footer
+	//previous song plays
+	//song name in list changes color
+	//if at first song, go to last
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
