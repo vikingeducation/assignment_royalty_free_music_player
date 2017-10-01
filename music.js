da9a9218dc9caeb057931d84112536faed1bd605
Visiona <darbis@gmail.com>
2017-10-01 23:30:42 +0100

    Created automatic refilling players list with spotify. TO DO - create authorized song playing

--- music.js
+++ music.js
@@ -21,7 +21,7 @@ $(document).ready( function() {
   }
 
   var xhr = createCORSRequest('GET', requestURL);
-  xhr.setRequestHeader('Authorization','Bearer BQBo8tixSHLUbBcVhkWH09-K_EX3g_q0vgu9UfvhFlfsuNgusa4dzhGHFdO2Tiium0BKVyRk62iuy2YOmw8FtD1jflKLgxOrUop8HidjKPmBJnbTLP9e1BOA4ldZylPyyBd2svm77FK3');
+  xhr.setRequestHeader('Authorization','Bearer BQBGQtPB-DD_9YPHHUuAl8bdbR7jTNaWnwomZaS7p3wLPS2S1D8XnbXWt2DzZuCFkK4d_HBJvbdWJXx5xUqTqIqHfmCCD_7Z6SNt2b4pAkCciDgqovMrIVxLRRMS505vcKKq2iov70Tv');
   xhr.responseType = 'json';
   xhr.send();
   // request.open('GET', requestURL);
@@ -35,20 +35,16 @@ $(document).ready( function() {
 
     for (var i = 0; i < albums.length; i++) {
       var albumSongs = albums[i]['tracks']['items'];
-
-      for (var j = 0; j < albumSongs.length; j++) {
+      for (var j = 0; j < 1; j++) {
         var currentSongName = albumSongs[j]['name'];
         var currentSongAuthor = albumSongs[j]['artists'][0]['name'];
-        var currentLink = albumSongs[j]['linked_from']['href'];
+        var currentLink = $(albumSongs[j]['linked_from']).attr('href');
 
         var $songBox = $('a.song-listed').clone();
-        $($songBox.get(0)).attr( 'id', 'song' + j + i );
-        $($songBox.get(0))[0]['childNodes'][5].text();
-        // zmien link source src=
-        $($songBox.get(0)).find('source').attr('src', currentLink )
+        $($songBox.get(0)).find('#mySong').attr( 'id', 'song' + j + i + Math.floor(Math.random()*10));
+        $($songBox.get(0)).find('source').attr('src', currentLink );
         $($songBox.get(0)).find('h5').text(currentSongName);
         $($songBox.get(0)).find('h6').text(currentSongAuthor);
-
         $songBox.appendTo($('div.playlist'));
       }
     }
@@ -57,12 +53,17 @@ $(document).ready( function() {
   $('span.glyphicon-pause').hide();
 
   var $control = $('#audioControl');
-  var aud = document.getElementById("mySong");
+  // var aud = document.getElementById('#mySong');
 
   $control.click( function() {
+    var aud = document.getElementById(this);
     $('span.glyphicon-pause').toggle();
     $('span.glyphicon-play').toggle();
     var pause = $('span.glyphicon-pause').is(':visible');
+
+    var zhr = new XDomainRequest();
+    zhr.open('GET', 'https://api.spotify.com/v1/tracks/3aByRUC2BVL6Fs1zI723sd');
+    zhr.setRequestHeader('Authorization','Bearer BQBGQtPB-DD_9YPHHUuAl8bdbR7jTNaWnwomZaS7p3wLPS2S1D8XnbXWt2DzZuCFkK4d_HBJvbdWJXx5xUqTqIqHfmCCD_7Z6SNt2b4pAkCciDgqovMrIVxLRRMS505vcKKq2iov70Tv');
 
     pause ? aud.play() : aud.pause() ;
 
