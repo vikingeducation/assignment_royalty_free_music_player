4c8a746ee382f8410516c687cd94a5511abbdc91
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-11 23:23:39 -0700

    added play/pause functionality

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-let sectionArray,
-    sectionArray;
-
 $(document).ready(function() {
-  sectionArray = $("section");
-  audioArray = $("audio");
 
-  //for(i = 0; i < sectionArray.length; i++) {
-  //  $(sectionArray[i].play).on
+  //listens for click on each song's play/pause button
+  $(".play").each((idx, el) => {
+    $(el).on("click", () => {
+      if ($(el).attr("src") === "images/play_button.png") {
+        $(el).attr("src", "images/pause.jpg");
+        $("audio").get(idx).play();
+      } else {
+        $(el).attr("src", "images/play_button.png");
+        $("audio").get(idx).pause();
+      }
+    })
+  })
 
-  $(".play").on("click", function () {
-
+  //listens for click on the footer play/pause button
+  $("#playPause").on("click", () => {
+    if ($("#playPause").attr("src") === "images/play_button.png") {
+      $("#playPause").attr("src", "images/pause.jpg");
+      $("audio").get(5).play();
+    } else {
+      $("#playPause").attr("src", "images/play_button.png");
+      $("audio").get(5).pause();
+    }
   });
 
-  changeSong(title, artist, file) {
+  /*changeSong(title, artist, file) {
     $("#currentSong").text(title);
     $("#currentArtist").text(artist);
     $("audio").attr("src", `audio/${songFile}`);
-  }
+  }*/
 
 
 });
