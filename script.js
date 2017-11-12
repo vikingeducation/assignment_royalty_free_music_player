8c83611e621bebc205637f2ca036c747f0175a74
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-12 00:15:11 -0700

    more jQuery additions

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
 
+  let songIndex = 0;
+
   //listens for click on each song's play/pause button
   $(".play").each((idx, el) => {
     $(el).on("click", () => {
       if ($(el).attr("src") === "images/play_button.png") {
         $(el).attr("src", "images/pause.jpg");
-        $("audio").get(idx).play();
+        $("#playPause").attr("src", "images/pause.jpg");
+        let newSong = $("audio").eq(idx).attr("src");
+        songIndex = idx;
+        if ($("#currentMusic").attr("src") !== newSong) {
+          $("#currentMusic").attr("src", newSong);
+        }
+        $(".songTitle h2").eq(idx).addClass(".current");
+        $("audio").get(5).play();
       } else {
         $(el).attr("src", "images/play_button.png");
-        $("audio").get(idx).pause();
+        $("#playPause").attr("src", "images/play_button.png");
+        $(".songTitle h2").eq(idx).removeClass(".current");
+        $("audio").get(5).pause();
       }
     })
   })
@@ -17,10 +28,42 @@ $(document).ready(function() {
   $("#playPause").on("click", () => {
     if ($("#playPause").attr("src") === "images/play_button.png") {
       $("#playPause").attr("src", "images/pause.jpg");
+      $(".play").eq(songIndex).attr("src", "images/pause.jpg");
+      $(".songTitle h2").eq(songIndex).addClass(".current");
       $("audio").get(5).play();
     } else {
       $("#playPause").attr("src", "images/play_button.png");
+      $(".play").eq(songIndex).attr("src", "images/play_button.png");
+      $(".songTitle h2").eq(songIndex).removeClass(".current");
       $("audio").get(5).pause();
+    }
+  });
+
+  //listens for click on previous button
+  $("#prev").on("click", () => {
+    if ($("#playPause").attr("src") === "images/pause.jpg") {
+      if (songIndex !== 0) {
+        songIndex = songIndex - 1;
+      } else {
+        songIndex = 4;
+      }
+      let newSong = $("audio").eq(songIndex).attr("src");
+      $("#currentMusic").attr("src", newSong);
+      $("audio").get(5).play();
+    }
+  });
+
+  //listens for click on next button
+  $("#next").on("click", () => {
+    if ($("#playPause").attr("src") === "images/pause.jpg") {
+      if (songIndex !== 4) {
+        songIndex = songIndex + 1;
+      } else {
+        songIndex = 0;
+      }
+      let newSong = $("audio").eq(songIndex).attr("src");
+      $("#currentMusic").attr("src", newSong);
+      $("audio").get(5).play();
     }
   });
 
