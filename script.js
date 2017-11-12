1c2f4a037110ca3dd55b933a69659dbe79fce80c
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-11 19:30:11 -0700

    updates to CSS and HTML

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
+let sectionArray,
+    sectionArray;
 
+$(document).ready(function() {
+  sectionArray = $("section");
+  audioArray = $("audio");
+
+  //for(i = 0; i < sectionArray.length; i++) {
+  //  $(sectionArray[i].play).on
+
+  $(".play").on("click", function () {
+
+  });
+
+  changeSong(title, artist, file) {
+    $("#currentSong").text(title);
+    $("#currentArtist").text(artist);
+    $("audio").attr("src", `audio/${songFile}`);
+  }
 
 
 });
