38f18fd1bbc130bad1319cbf80fc5f0b8c869447
tzvi <tzvika200@yahoo.com>
2017-11-16 12:37:31 -0500

    refactored. seems to be working ok

--- README.md
+++
@@ -1,4 +0,0 @@
-# assignment_royalty_free_music_player
-Ushering in the reign of Royalty Free Music w/ JavaScript.
-
-Tzvi Seliger
--- js/collections.js
+++ js/collections.js
@@ -17,32 +17,32 @@ songs = [ "anewbeginning.mp3",
 		 ]
 //Build the HTML //
 function buildHTML() {
-	$(`.${page}`).prepend(`<ul class = '${ulPlay}'></ul>`)
+	$(`.page`).prepend(`<ul class = 'ulPlay'></ul>`)
 	songs.forEach((item) => {
-		$(`.${ulPlay}`).append(`<li class = '${liPlay} full-width'></li>`)
+		$(`.ulPlay`).append(`<li class = 'liPlay full-width'></li>`)
 	})
-	$(`.${liPlay}`).append(`<div class = '${divClass}'></div>`)
-	$(`.${divClass}`).append(`<p class = '${playClass}'></p>`)
-	$(`.${divClass}`).append(`<img src = 'assets/media/images/play.svg'/>`)
+	$(`.liPlay`).append(`<div class = 'divClass'></div>`)
+	$(`.divClass`).append(`<p class = 'playClass'></p>`)
+	$(`.divClass`).append(`<img src = 'assets/media/images/play.svg'/>`)
 	songs.forEach(function(item, index) {
-		$(`.${divClass}`).eq(index).append(`<audio src = '${file_path + item}' id =
+		$(`.divClass`).eq(index).append(`<audio src = '${file_path + item}' id =
 		 'audio${index}' class = 'audio'></audio>`)
 	})
 	songs.forEach((item, index) => {
-		$(`.${divClass}`).eq(index).append(`<p>${item}</p>`)
+		$(`.divClass`).eq(index).append(`<p>${item}</p>`)
 	})
 }
 //End Build the HTML //
 //Audio Settings//
 function audioSettings() {
+	//attach listeners on all "divs", when clicked the corresponding "audio" item should call the togglePlay
+	//so it checks what should be stopped and started
 	action(L(".divClass"), ".audio", loop, "click", togglePlay)
 	L("#pause").addEventListener("click", function() {
 		current_playing.pause()
 	})
 	L("#play").addEventListener("click", function() {
-		if (current_playing === "") {
-			current_playing = document.getElementById("audio0")
-		}
+		if (!current_playing) current_playing = $("#audio0").get(0)
 		current_playing.play()
 	})
 	L("#fwd").addEventListener("click", fwd)
@@ -54,12 +54,13 @@ function styling() {
 	$("#pause").hide()
 	$("#play").on("click", () => play())
 	$("#pause").on("click", () => pause())
-	$("li").on("click", function(e) {
-		play()
+	setCurrentPlayingItemStyle("li", "click", function() {
 		$("li").css("background", "white")
 		$(this).css("background", "aqua")
 		$(".bottom-banner p").remove()
-		$(".bottom-banner").append(`<p class = ${ bannerP} >${this.innerText}</p>`)
+		$(".bottom-banner").append(`<p class = 'bannerP'>${this.innerText}</p>`)
+		console.log(this)
 	})
+	//play()
 }
 //End jQuery Styling//
