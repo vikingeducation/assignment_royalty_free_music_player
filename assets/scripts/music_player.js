714c998a6f1df2d681a254c93801e68fd597cdf4
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-04 00:48:39 -0400

    setup for js portion

---
+++ assets/scripts/music_player.js
@@ -0,0 +1,36 @@
+
+/*
+
+cd Documents/Viking/JS/music_player
+
+cd Documents/Viking/JS/music_player/assets/styles
+
+sass --watch input.scss:style.css
+
+sass input.scss style.css
+
+TODO
+
+1. js button and text swapping
+
+2. find actual songs - https://freemusicarchive.org/
+
+3. update html to match songs
+
+4. js to actually play/stop songs
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// spacing
