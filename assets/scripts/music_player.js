b0d3721799e87214e9b148373ceaeadf243ddad3
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-11 21:09:44 -0400

    finished status bar play button event

--- assets/scripts/music_player.js
+++ assets/scripts/music_player.js
@@ -1,25 +1,23 @@
 
 /*
+  cd Documents/Viking/JS/music_player
 
-cd Documents/Viking/JS/music_player
+  cd Documents/Viking/JS/music_player/assets/styles
 
-cd Documents/Viking/JS/music_player/assets/styles
+  sass --watch input.scss:style.css
 
-sass --watch input.scss:style.css
+  sass input.scss style.css
 
-sass input.scss style.css
+  TODO
+  1. js button and text swapping
+    a. look into functions for repeated actions and maybe global variables for
+       repeated selections/other methods to reduce repetition
 
-TODO
-1. js button and text swapping
-  a. look into functions for repeated actions and maybe global variables for
-     repeated selections
+  2. find actual songs - https://freemusicarchive.org/
 
-2. find actual songs - https://freemusicarchive.org/
+  3. update html to match songs
 
-3. update html to match songs
-
-4. js to actually play/stop songs
-
+  4. js to actually play/stop songs
 */
 
 /* song play button */
@@ -28,11 +26,11 @@ var counter = 0;
 
 while (counter < 5) {
   songPlay[counter].addEventListener("click", function(action) {
-// reset previous song
+    // reset previous song
     var hidden = document.getElementsByClassName("song-play hide")[0];
     var played = document.getElementsByClassName("song-stop playing")[0];
 
-    if (hidden != undefined ) {
+    if (hidden != undefined) {
       hidden.classList.remove("hide");
     }
 
@@ -40,69 +38,77 @@ while (counter < 5) {
       played.classList.remove("playing");
     }
 
-// song button change
+    // song button change
     var source = action.target;
     var siblings = source.parentNode.children;
 
     source.classList.add("hide");
     siblings[1].classList.add("playing");
 
-// status bar button change
+    // status bar button change
     document.getElementsByClassName("status-play")[0].classList.add("hide");
     document.getElementsByClassName("status-pause")[0].classList.add("playing");
 
-// status bar text change
+    // status bar text change
     document.querySelectorAll("h4")[0].innerHTML = siblings[2].innerHTML;
     document.querySelectorAll("h5")[0].innerHTML = siblings[3].innerHTML;
-/*
-  TODO
-  actually play song
-*/
+
+    /*
+      TODO
+      actually play song
+    */
   });
-  counter++
+  counter++;
 }
 /* song play button */
 
 /* status play button */
-  /*
-    TODO
-    1. status button change
-
-    2. find song that matches text
-
-    3. that songs button changes
-
-    4. comment song playing for later
-  */
 var statusPlay = document.getElementsByClassName("status-play")[0];
 
 statusPlay.addEventListener("click", function(action) {
-
-// status bar button change
+  // status bar button change
   statusPlay.classList.add("hide");
   document.getElementsByClassName("status-pause")[0].classList.add("playing");
 
-// find selected song
+  // find selected song
   var statusSong = document.querySelectorAll("h4")[0].innerHTML;
   var statusArtist = document.querySelectorAll("h5")[0].innerHTML;
+  var songs = document.getElementsByClassName("song");
+  var tracker = 0;
 
-/*
-  1. grab html collection of songs
+  while (tracker < 5) {
+    if (
+      songs[tracker].children[2].innerHTML == statusSong &&
+      songs[tracker].children[3].innerHTML == statusArtist
+    ) {
+      var selectedSong = songs[tracker].children;
+      break;
+    }
+    tracker++;
+  }
 
-  2. loop through said collection with an if statement that matches innerhtml
-   contents, using .children and the fact that the h2 and h3 elements are
-   consitently located
+  // song button change
+  selectedSong[0].classList.add("hide");
+  selectedSong[1].classList.add("playing");
 
-  3. save correct song to a variable to later change its button and play the
-  song
-*/
-
-  // songPlay
-
-
-
+  /*
+    TODO
+    actually play song
+  */
 });
 /* status play button */
+
+/* song stop button */
+/* song stop button */
+
+/* status pause button */
+/* status pause button */
+
+/* status previous button */
+/* status previous button */
+
+/* status next button */
+/* status next button */
 
 
 
